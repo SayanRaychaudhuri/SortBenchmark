4e0060a1799c526f4d401da01500e99d3c615703
SayanRaychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-23 00:51:45 -0500

    Make main recursive to repeat print format

--- src/Tester.java
+++ src/Tester.java
@@ -3,8 +3,9 @@ public class Tester {
     static Sorting sorting = new Sorting();
     public static void main(String[] args) {
         System.out.printf("%s%15s%15s%15s%15s%15s\n", "selection", "insertion", "merge", "bubble", "quick", "radix");
-        System.out.printf("%d%15d%15d%15d%15d%15d", sorting.selectionSortAvgTime(), sorting.insertionSortAvgTime()
+        System.out.printf("%d%15d%15d%15d%15d%15d\n", sorting.selectionSortAvgTime(), sorting.insertionSortAvgTime()
                                                     , sorting.mergeSortAvgTime(), sorting.bubbleSortAvgTime()
                                                     , sorting.quickSortAvgTime(), sorting.radixSortAvgTime());
+        main(null);
     }
 }
